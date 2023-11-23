1eed300b45edccfa12b2a715b94da2fffa60fbc3
SuperSamedi <contact.max.dolmans@gmail.com>
2023-11-23 15:54:56 +0100

    Aménagement page d'accueil

---
+++ index.js
@@ -0,0 +1,5 @@
+const quizCardMana = document.getElementById('le-grand-test-du-mana');
+
+quizCardMana.addEventListener('click', e => {
+    window.location.href = 'le-grand-test-du-mana/home.html';
+});
